1bfd1df1362fabc3f9d5c9787e8436ce4280e6ce
Luke Hsiao <lwhsiao@stanford.edu>
2020-07-20 23:53:29 -0700

    feat(bin/recording): add run_calibration()

--- fvideo/src/bin/recording.rs
+++ fvideo/src/bin/recording.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use std::process;
 
 use anyhow::{anyhow, Result};
+use eyelink_rs::libeyelink_sys;
 use log::{error, info};
 use structopt::clap::AppSettings;
 use structopt::StructOpt;
@@ -76,8 +77,18 @@ fn initialize_eyelink(opt: &Opt) -> Result<()> {
     }
     eyelink_rs::eyecmd_printf("add_file_preamble_text 'RECORDED BY recording.rs'")?;
 
+    // Initialize SDL-based graphics
+    let mut disp = eyelink_rs::get_display_information();
+    eyelink_rs::init_expt_graphics(&mut disp)?;
+
     // Set display resolution
-    eyelink_rs::eyecmd_printf("screen_pixel_coords = 0 0 1920 1080")?;
+    eyelink_rs::eyecmd_printf(
+        format!(
+            "screen_pixel_coords = {} {} {} {}",
+            disp.left, disp.top, disp.right, disp.bottom
+        )
+        .as_str(),
+    )?;
 
     let (version, sw_version) = eyelink_rs::eyelink_get_tracker_version()?;
 
@@ -133,6 +144,33 @@ fn initialize_eyelink(opt: &Opt) -> Result<()> {
 
 /// Run a 9-point eyelink calibration
 fn run_calibration() -> Result<()> {
+    let mut target_fg_color: libeyelink_sys::SDL_Color = libeyelink_sys::SDL_Color {
+        r: 0,
+        g: 0,
+        b: 0,
+        unused: 255,
+    };
+    let mut target_bg_color: libeyelink_sys::SDL_Color = libeyelink_sys::SDL_Color {
+        r: 200,
+        g: 200,
+        b: 200,
+        unused: 255,
+    };
+
+    eyelink_rs::set_calibration_colors(&mut target_fg_color, &mut target_bg_color);
+
+    eyelink_rs::do_tracker_setup();
+
+    // If ESC was pressed, repeat drift correction
+    loop {
+        // Create screen to bg color, draw target, clear again when done, and
+        // allow ESC to access setup menu before returning, rather than abort.
+        match eyelink_rs::do_drift_correct(1920 / 2, 1080 / 2, true, true) {
+            Err(eyelink_rs::EyelinkError::EscPressed) => continue,
+            _ => break,
+        }
+    }
+
     Ok(())
 }
 
