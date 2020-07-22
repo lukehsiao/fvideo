b3eed543dcb0e83ee8a6279e6354dba74cc00622
Luke Hsiao <lwhsiao@stanford.edu>
2020-07-22 00:28:26 -0700

    refactor(bin/recording): simplify video playback

--- fvideo/src/bin/recording.rs
+++ fvideo/src/bin/recording.rs
@@ -204,113 +204,106 @@ fn play_video(opt: &Opt) -> Result<()> {
     // Use ffmpeg to decode
     ffmpeg_next::init()?;
 
-    if let Ok(mut ictx) = input(&opt.video) {
-        let in_stream = ictx
-            .streams()
-            .best(Type::Video)
-            .ok_or_else(|| ffmpeg_next::Error::StreamNotFound)?;
+    let mut ictx = input(&opt.video)?;
+    let in_stream = ictx
+        .streams()
+        .best(Type::Video)
+        .ok_or_else(|| ffmpeg_next::Error::StreamNotFound)?;
 
-        let mut video_decoder = in_stream.codec().decoder().video()?;
+    let mut video_decoder = in_stream.codec().decoder().video()?;
 
-        let mut scaler = Scaler::get(
-            video_decoder.format(),
+    let mut scaler = Scaler::get(
+        video_decoder.format(),
+        video_decoder.width(),
+        video_decoder.height(),
+        Pixel::YUV420P,
+        video_decoder.width(),
+        video_decoder.height(),
+        Flags::BILINEAR,
+    )?;
+
+    let sdl_context = sdl2::init().map_err(|e| anyhow!(e))?;
+    let video_subsystem = sdl_context.video().map_err(|e| anyhow!(e))?;
+
+    let window = video_subsystem
+        .window(
+            "recording.rs",
             video_decoder.width(),
             video_decoder.height(),
-            Pixel::YUV420P,
-            video_decoder.width(),
-            video_decoder.height(),
-            Flags::BILINEAR,
-        )?;
+        )
+        .position_centered()
+        .build()?;
 
-        let sdl_context = sdl2::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
+    let mut canvas = window
+        .into_canvas()
+        .accelerated()
+        .present_vsync()
+        .target_texture()
+        .build()?;
 
-        match video_subsystem.gl_set_swap_interval(1) {
-            Ok(_) => {}
-            _ => println!("error occurred during setting of swap interval"),
-        }
+    video_subsystem
+        .gl_set_swap_interval(1)
+        .map_err(|e| anyhow!("Failed setting swap: {}", e))?;
 
-        let window = video_subsystem
-            .window("spe", video_decoder.width(), video_decoder.height())
-            .position_centered()
-            .build()
-            .unwrap();
+    let texture_creator = canvas.texture_creator();
+    let mut texture = texture_creator.create_texture_streaming(
+        PixelFormatEnum::YV12,
+        video_decoder.width(),
+        video_decoder.height(),
+    )?;
 
-        let mut canvas = window
-            .into_canvas()
-            .accelerated()
-            .present_vsync()
-            .target_texture()
-            .build()
-            .unwrap();
+    let mut prev_pts = None;
+    let mut now = std::time::Instant::now();
+    for (i, (stream, packet)) in ictx.packets().enumerate() {
+        match stream.codec().codec() {
+            Some(codec) if codec.is_video() => {
+                let mut frame = Video::empty();
+                match video_decoder.decode(&packet, &mut frame) {
+                    Ok(_) => {
+                        let mut yuv_frame = Video::empty();
+                        scaler.run(&frame, &mut yuv_frame)?;
 
-        let texture_creator = canvas.texture_creator();
-        let mut texture = texture_creator
-            .create_texture_streaming(
-                PixelFormatEnum::YV12,
-                video_decoder.width(),
-                video_decoder.height(),
-            )
-            .unwrap();
+                        let rect = Rect::new(0, 0, yuv_frame.width(), yuv_frame.height());
+                        info!("rendering frame {}", i);
+                        let _ = texture.update_yuv(
+                            rect,
+                            yuv_frame.data(0),
+                            yuv_frame.stride(0),
+                            yuv_frame.data(1),
+                            yuv_frame.stride(1),
+                            yuv_frame.data(2),
+                            yuv_frame.stride(2),
+                        );
 
-        let mut prev_pts = None;
-        let mut now = std::time::Instant::now();
-        for (i, (stream, packet)) in ictx.packets().enumerate() {
-            match stream.codec().codec() {
-                Some(codec) if codec.is_video() => {
-                    let mut frame = Video::empty();
-                    match video_decoder.decode(&packet, &mut frame) {
-                        Ok(_) => {
-                            let mut yuv_frame = Video::empty();
-                            scaler.run(&frame, &mut yuv_frame)?;
+                        canvas.clear();
+                        let _ = canvas.copy(&texture, None, None); //copy texture to our canvas
+                        canvas.present();
 
-                            let rect = Rect::new(0, 0, yuv_frame.width(), yuv_frame.height());
-                            println!("rendering frame {}", i);
-                            let _ = texture.update_yuv(
-                                rect,
-                                yuv_frame.data(0),
-                                yuv_frame.stride(0),
-                                yuv_frame.data(1),
-                                yuv_frame.stride(1),
-                                yuv_frame.data(2),
-                                yuv_frame.stride(2),
-                            );
-
-                            canvas.clear();
-                            let _ = canvas.copy(&texture, None, None); //copy texture to our canvas
-                            canvas.present();
-
-                            let pts = (Rational64::from(packet.pts().unwrap() * 1000000000)
-                                * Rational64::new(
-                                    stream.time_base().numerator() as i64,
-                                    stream.time_base().denominator() as i64,
-                                ))
-                            .to_integer();
-                            //let pts = p.pts().unwrap();
-                            println!("pts - {}, timebase - {}", pts, stream.time_base());
-                            if let Some(prev) = prev_pts {
-                                let elapsed = now.elapsed();
-                                if pts > prev {
-                                    let sleep = time::Duration::new(0, (pts - prev) as u32);
-                                    println!("sleep - {:?}, elapsed - {:?}", sleep, elapsed);
-                                    if elapsed < sleep {
-                                        println!("sleeping ... ");
-                                        std::thread::sleep(sleep - elapsed);
-                                    }
+                        let pts = (Rational64::from(packet.pts().unwrap() * 1000000000)
+                            * Rational64::new(
+                                stream.time_base().numerator() as i64,
+                                stream.time_base().denominator() as i64,
+                            ))
+                        .to_integer();
+                        if let Some(prev) = prev_pts {
+                            let elapsed = now.elapsed();
+                            if pts > prev {
+                                let sleep = time::Duration::new(0, (pts - prev) as u32);
+                                if elapsed < sleep {
+                                    std::thread::sleep(sleep - elapsed);
                                 }
                             }
+                        }
 
-                            now = time::Instant::now();
-                            println!("now - {:?}", now.elapsed());
-                            prev_pts = Some(pts);
-                        }
-                        _ => {
-                            println!("Error occurred while decoding packet.");
-                        }
+                        now = time::Instant::now();
+                        prev_pts = Some(pts);
+                    }
+                    _ => {
+                        error!("Error occurred while decoding packet.");
                     }
                 }
-                _ => {}
             }
+            _ => {}
         }
     }
     Ok(())
