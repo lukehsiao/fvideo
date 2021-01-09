lukehsiao/fvideo

commit 222926cebf0dac4a94e8936237c4c197b4c2c250

refactor(fvideo/client): use an RGBA texture to alpha blending
Uses ffmpeg to convert the yuv420p decoded frame into an ABGR frame.
This appears to be the required format to correctly update into an
RGBA8888 SDL2 texture.

This extra colorspace conversion (which does not have a special
accelerated version), drops the FPS from ~450fps to ~380fps on my
machine. A fairly significant performance hit (with f=10).

Using this new RGBA texture, we can blend the foreground with the
background. For this implementation, I just use a 2D Guassian
hand-tweaked until I felt it looked OK. In particular, I wanted a fairly
large section of the foreground to be maxed at 255, with a quick
drop-off on the edges. You can see what this contour looks like for f=10
by plotting the following in Wolfram Alpha:

    Plot3D[min(255, 512* e^(-(((x - 80)^2 + (y - 80)^2) / (2 * (160/3.5)^2)))), {x, 0, 160}, {y, 0, 160}]

The additional operations to compute these alpha values does not
introduce any significant performance hit compared to the colorspace
conversion.

fvideo/src/client.rs
@@ -5,19 +5,21 @@
 
 extern crate ffmpeg_next as ffmpeg;
 
+use std::cmp;
 use std::collections::VecDeque;
 use std::convert::TryInto;
 use std::path::PathBuf;
 use std::process;
 use std::time::Instant;
 
+use ffmpeg::util::format::pixel::Pixel;
 use ffmpeg::util::frame::video::Video;
 use ffmpeg::{codec, decoder, Packet};
 use log::{debug, error, info};
 use sdl2::event::EventType;
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
-use sdl2::render::{Canvas, TextureCreator};
+use sdl2::render::{BlendMode, Canvas, TextureCreator};
 use sdl2::video::{Window, WindowContext};
 use sdl2::EventPump;
 
@@ -493,13 +495,9 @@ impl FvideoClient {
 
         let mut fg_texture = self
             .texture_creator
-            .create_texture_streaming(PixelFormatEnum::YV12, self.fg_width, self.fg_height)
+            .create_texture_streaming(PixelFormatEnum::RGBA8888, self.fg_width, self.fg_height)
             .unwrap();
-        // TODO(lukehsiao): Create a new texture mask the same size as fg_texture with the 2d
-        // gaussian, then blend the fg_texture with that one in additive(?) mode to get the 2d alpha
-        // channels set correctly, then just copy to the renderer as normal.
-        // fg_texture.set_blend_mode(BlendMode::Blend);
-        // fg_texture.set_alpha_mod(200);
+        fg_texture.set_blend_mode(BlendMode::Blend);
 
         let mut bg_texture = self
             .texture_creator
@@ -518,6 +516,7 @@ impl FvideoClient {
         self.total_bytes += fg_packet.size() as u64 + bg_packet.size() as u64;
         // self.total_bytes += fg_packet.size() as u64;
         let mut fg_frame = Video::empty();
+        let mut fg_frame_rgba = Video::empty();
         let mut bg_frame = Video::empty();
         match (
             self.fg_decoder.decode(&fg_packet, &mut fg_frame),
@@ -533,15 +532,34 @@ impl FvideoClient {
                 let time = Instant::now();
                 let fg_rect = Rect::new(0, 0, fg_frame.width(), fg_frame.height());
                 let bg_rect = Rect::new(0, 0, bg_frame.width(), bg_frame.height());
-                let _ = fg_texture.update_yuv(
-                    fg_rect,
-                    fg_frame.data(0),
-                    fg_frame.stride(0),
-                    fg_frame.data(1),
-                    fg_frame.stride(1),
-                    fg_frame.data(2),
-                    fg_frame.stride(2),
-                );
+
+                let mut converter = fg_frame.converter(Pixel::ABGR).unwrap();
+                converter.run(&fg_frame, &mut fg_frame_rgba).unwrap();
+
+                // Manipulate the alpha channel to give blend at the edges
+                let height = fg_frame_rgba.height();
+                let width = fg_frame_rgba.stride(0);
+                let rgba_data = fg_frame_rgba.data_mut(0);
+
+                // Set the alpha values based on a circular 2D Gaussian. These constants right now
+                // are just tuned to what seems to look OK to me. See commit msg for details.
+                for j in 0..height {
+                    for i in (0..width).step_by(4) {
+                        rgba_data[(width * j as usize) + i] = cmp::min(
+                            255,
+                            (512.0
+                                * (-1.0
+                                    * ((((i / 4) as i32 - (height / 2) as i32).pow(2)
+                                        + (j as i32 - (height / 2) as i32).pow(2))
+                                        as f32
+                                        / (2.0 * (height as f32 / 3.5).powi(2))))
+                                .exp())
+                            .round() as u8,
+                        );
+                    }
+                }
+
+                let _ = fg_texture.update(fg_rect, fg_frame_rgba.data(0), fg_frame_rgba.stride(0));
 
                 let _ = bg_texture.update_yuv(
                     bg_rect,

commit 8275bca43a58cf7f6111bbd502be0fb8b81e0e8d

perf(fvideo/client): avoid unaccelerated colorspace conversion
When introducing the RGBA texture for alpha blending, we introduced a
colorspace conversion that ffmpeg did not accelerate (yuv420p to abgr).
This unaccelerated conversion can be easily replaced with an accelerated
version (yuv420p to rgba) by using the correct pixel format for the
foreground texture. Somewhat counterintuitively, this is ABGR8888.

It is unclear to me why these appear to be inverses of each other, but
it is functional and the performance drop incurred previously is
successfully avoided.

Fixes: 222926cebf0dac4a94e8936237c4c197b4c2c250

fvideo/src/client.rs
@@ -516,7 +516,7 @@ impl FvideoClient {
 
         let mut fg_texture = self
             .texture_creator
-            .create_texture_streaming(PixelFormatEnum::RGBA8888, self.fg_width, self.fg_height)
+            .create_texture_streaming(PixelFormatEnum::ABGR8888, self.fg_width, self.fg_height)
             .unwrap();
         fg_texture.set_blend_mode(BlendMode::Blend);
 
@@ -554,7 +554,7 @@ impl FvideoClient {
                 let fg_rect = Rect::new(0, 0, fg_frame.width(), fg_frame.height());
                 let bg_rect = Rect::new(0, 0, bg_frame.width(), bg_frame.height());
 
-                let mut converter = fg_frame.converter(Pixel::ABGR).unwrap();
+                let mut converter = fg_frame.converter(Pixel::RGBA).unwrap();
                 converter.run(&fg_frame, &mut fg_frame_rgba).unwrap();
 
                 // Manipulate the alpha channel to give blend at the edges
@@ -565,7 +565,7 @@ impl FvideoClient {
                 let mut alpha_iter = self.alpha_blend.iter();
                 for j in 0..height {
                     for i in (0..width).step_by(4) {
-                        rgba_data[(width * j as usize) + i] = *alpha_iter.next().unwrap();
+                        rgba_data[(width * j as usize) + i + 3] = *alpha_iter.next().unwrap();
                     }
                 }
 