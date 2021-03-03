afc356e79488d6f8a56ef4c9b798572072d12c07
Luke Hsiao <luke@hsiao.dev>
2021-03-03 15:32:11 -0800

    feat: log bg_width, bg_qp, and fg_qp in results

--- fvideo/src/bin/realtime.rs
+++ fvideo/src/bin/realtime.rs
@@ -232,13 +232,16 @@ fn main() -> Result<()> {
     let mut cfg_dest = BufWriter::new(fs::File::create(cfg_dest)?);
     writeln!(
         cfg_dest,
-        "alg,fovea,qo_max,gaze_source,video,frames,elapsed_time,fps,filesize_bytes",
+        "alg,fovea,bg_width,bg_qp,fg_qp,qo_max,gaze_source,video,frames,elapsed_time,fps,filesize_bytes",
     )?;
     write!(
         cfg_dest,
-        "{},{},{},{},{},",
+        "{},{},{},{},{},{},{},{},",
         opt.alg,
         opt.fovea,
+        opt.bg_width,
+        opt.bg_qp,
+        opt.fg_qp,
         opt.qo_max,
         opt.gaze_source,
         opt.video.display()
